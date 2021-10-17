e5bace7edb24719c6a79068bceef090041c82b72
Onique Campbell <87822768+Onique-Unique@users.noreply.github.com>
2021-10-17 10:33:49 -0400

    Update Speed_Test_Project.py

--- Speed_Test_Project.py
+++ Speed_Test_Project.py
@@ -4,36 +4,40 @@ wifi  = speedtest.Speedtest()
 import time
 
 counter= 0
+print("Welcome to Network Speedtest")
+while True:
+    print("Press Enter to Run Speed Test")
+    Press = input()
 
-print("Speed Test is Running - Please Wait")
+    print("Speed Test is Running - Please Wait")
 
-DownloadSpeed = round(wifi.download(), 2)
+    DownloadSpeed = round(wifi.download(), 2)
 
-for counter, x in enumerate(list(range(101))):
-    counter += 1
-    time.sleep(0.08)
-    if counter == 101:
-        break
-    print("Gathering Download Speed:", counter,"%", end="\r")
+    for counter, x in enumerate(list(range(101))):
+        counter += 1
+        time.sleep(0.08)
+        if counter == 101:
+            break
+        print("Gathering Download Speed:", counter,"%", end="\r")
 
-print("\n")
-print("Finalizing Speed Test - Please Wait")
+    print("\n")
+    print("Finalizing Speed Test - Please Wait")
 
-UploadSpeed = round(wifi.upload(), 2)
+    UploadSpeed = round(wifi.upload(), 2)
 
-for counter, x in enumerate(list(range(101))):
-    counter += 1
-    time.sleep(0.08)
-    if counter == 101:
-        break
-    print("Gathering Upload Speed:", counter,"%", end="\r")
+    for counter, x in enumerate(list(range(101))):
+        counter += 1
+        time.sleep(0.08)
+        if counter == 101:
+            break
+        print("Gathering Upload Speed:", counter,"%", end="\r")
 
-print("\n")
+    print("\n")
 
-#1000000 is represented as 1 million kbs which equates to 1  gbs
+    #1000000 is represented as 1 million kbs which equates to 1  gbs
 
-Download_Speed = DownloadSpeed // 1000000
-Upload_Speed = UploadSpeed // 1000000 
+    Download_Speed = DownloadSpeed // 1000000
+    Upload_Speed = UploadSpeed // 1000000 
 
-print("Wifi Download Speed is ", Download_Speed, "GB's")
-print("Wifi Upload Speed is ", Upload_Speed, "GB's")
+    print("Wifi Download Speed is ", Download_Speed, "GB's")
+    print("Wifi Upload Speed is ", Upload_Speed, "GB's")
